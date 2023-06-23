88d41ff8aa5d904ed0e6ccd62873e5f1d2eb92a6
mikesukim <mikesungunkim@gmail.com>
2023-06-22 23:42:04 -0700

    add firebase initialization at main

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,17 @@ import 'package:dineseater_client_gilson/app/app.locator.dart';
 import 'package:dineseater_client_gilson/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
   setupDialogUi();
   setupBottomSheetUi();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
