e1608cce54904c6fdbf154505d95b64214d2821b
Jiyun Noh Kim <jiyunnoh1127@gmail.com>
2023-10-04 16:03:12 -0700

    Update startup_view.dart

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter_device_type/flutter_device_type.dart';
 import 'package:stacked/stacked.dart';
-import 'package:dineseater_client_gilson/ui/common/ui_helpers.dart';
 
 import '../../common/app_colors.dart';
 import 'startup_viewmodel.dart';
@@ -19,22 +17,17 @@ class StartupView extends StackedView<StartupViewModel> {
     return Scaffold(
       backgroundColor: kcPrimaryColor,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(bottom: Device.get().isTablet ? 20.0 : 0.0),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                verticalSpaceLarge,
-                Image.asset('assets/Logo_Gilson.png'),
-                Image.asset(
-                  'assets/Logo_DineSeater_white.png',
-                  width: 75,
-                ),
-              ],
+        child: Stack(
+          children: [
+            Center(child: Image.asset('assets/Logo_Gilson.png')),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                'assets/Logo_DineSeater_white.png',
+                width: 75,
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
