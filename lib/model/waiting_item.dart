0c13441b5ff653954355e0dffb17548dffd31151
mikesukim <mikesungunkim@gmail.com>
2023-12-29 03:56:40 -0800

    feat: add getWaitingList & model classes (waiting_list_response & waiting_item)

---
+++ lib/model/waiting_item.dart
@@ -0,0 +1,50 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'waiting_item.g.dart';
+
+@JsonSerializable()
+class WaitingItem {
+  @JsonKey(name: 'business_name')
+  late String businessName;
+
+  @JsonKey(name: 'waiting_id')
+  late String waitingId;
+
+  @JsonKey(name: 'detail_attribute')
+  late DetailAttribute detailAttribute;
+
+  @JsonKey(name: 'status')
+  late String status;
+
+  @JsonKey(name: 'phone_number')
+  late String phoneNumber;
+
+  @JsonKey(name: 'number_of_customers')
+  late int numberOfCustomers;
+
+  @JsonKey(name: 'date_created')
+  late String dateCreated;
+
+  WaitingItem();
+
+  factory WaitingItem.fromJson(Map<String, dynamic> json) =>
+      _$WaitingItemFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WaitingItemToJson(this);
+}
+
+@JsonSerializable()
+class DetailAttribute {
+  @JsonKey(name: 'is_grill')
+  late bool isGrill;
+
+  @JsonKey(name: 'is_meal')
+  late bool isMeal;
+
+  DetailAttribute();
+
+  factory DetailAttribute.fromJson(Map<String, dynamic> json) =>
+      _$DetailAttributeFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DetailAttributeToJson(this);
+}
--- lib/services/dineseater_api_service.dart
+++ lib/services/dineseater_api_service.dart
@@ -1,10 +1,29 @@
+import 'dart:convert';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:logger/logger.dart';
 
 import '../app/app.locator.dart';
+import '../model/waiting_list_response.dart';
 import 'cognito_service.dart';
 
+import 'package:http/http.dart' as http;
+
 class DineseaterApiService {
+  final logger = Logger();
   final _cognitoService = locator<CognitoService>();
   final String _baseUrl = dotenv.env['DINESEATER_API_URL']!;
 
+  Future<WaitingListResponse> getWaitingList() async {
+    final token = await _cognitoService.getIdToken();
+    final url = Uri.parse('$_baseUrl/business/waitinglist');
+    final response = await http.get(url, headers: {'Authorization': token!});
+
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+      return WaitingListResponse.fromJson(jsonResponse);
+    } else {
+      throw Exception('Failed to load waiting list');
+    }
+  }
 }
