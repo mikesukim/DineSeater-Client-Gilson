4c2b5b90ccb6c564d7f4bb8a8d7816ba2496532f
mikesukim <mikesungunkim@gmail.com>
2023-12-29 04:31:23 -0800

    chore : add logging to getWaitingList()

--- lib/services/dineseater_api_service.dart
+++ lib/services/dineseater_api_service.dart
@@ -15,14 +15,21 @@ class DineseaterApiService {
   final String _baseUrl = dotenv.env['DINESEATER_API_URL']!;
 
   Future<WaitingListResponse> getWaitingList() async {
+    logger.i('getWaitingList');
     final token = await _cognitoService.getIdToken();
     final url = Uri.parse('$_baseUrl/business/waitinglist');
-    final response = await http.get(url, headers: {'Authorization': token!});
-
+    final http.Response response;
+    try {
+      response = await http.get(url, headers: {'Authorization': token!});
+    } catch (e) {
+      logger.e('error while requesting waiting list: $e');
+      rethrow;
+    }
     if (response.statusCode == 200) {
       final jsonResponse = json.decode(response.body);
       return WaitingListResponse.fromJson(jsonResponse);
     } else {
+      logger.e('Failed to load waiting list with status code: ${response.statusCode}');
       throw Exception('Failed to load waiting list');
     }
   }
