62edc5b903fdd25a63a0db2be8abcea61327f34c
mikesukim <mikesungunkim@gmail.com>
2023-12-30 21:06:11 -0800

    feat : add reactive value to waiting_storage_service.dart

--- lib/services/waiting_storage_service.dart
+++ lib/services/waiting_storage_service.dart
@@ -1,1 +1,27 @@
-class WaitingStorageService {}
+import 'package:dineseater_client_gilson/model/waiting_item.dart';
+import 'package:stacked/stacked.dart';
+
+class WaitingStorageService with ListenableServiceMixin {
+  // TODO : consider to switch to map for better performance
+  final ReactiveValue<List<WaitingItem>> _waitings =
+  ReactiveValue<List<WaitingItem>>([]);
+
+  WaitingStorageService() {
+    listenToReactiveValues([_waitings]);
+  }
+
+  List<WaitingItem> get waitings => _waitings.value;
+
+  void addWaiting(WaitingItem waiting) {
+    // TODO : add to local storage
+    _waitings.value = [..._waitings.value, waiting];
+  }
+
+  void removeWaiting(String waitingId) {
+    // TODO : remove from local storage as well
+    _waitings.value = _waitings.value
+        .where((element) => element.waitingId != waitingId)
+        .toList();
+  }
+}
+
