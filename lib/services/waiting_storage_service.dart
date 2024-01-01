80d6fbe1153131ccac047a767e961afbfc981102
mikesukim <mikesungunkim@gmail.com>
2024-01-01 12:26:26 -0800

    feat : update waiting_storage_service to ReactiveList and add addWaiting & removeWaiting

--- lib/services/waiting_storage_service.dart
+++ lib/services/waiting_storage_service.dart
@@ -1,27 +1,74 @@
 import 'package:dineseater_client_gilson/model/waiting_item.dart';
+import 'package:localstorage/localstorage.dart';
+import 'package:logger/logger.dart';
 import 'package:stacked/stacked.dart';
 
 class WaitingStorageService with ListenableServiceMixin {
-  // TODO : consider to switch to map for better performance
-  final ReactiveValue<List<WaitingItem>> _waitings =
-  ReactiveValue<List<WaitingItem>>([]);
+  var logger = Logger();
+  late ReactiveList<WaitingItem> _waitings =
+      ReactiveList<WaitingItem>();
+  final LocalStorage _waitingStorage = LocalStorage('waitingStorage');
+  final String _waitingsKey = 'waitings';
 
   WaitingStorageService() {
     listenToReactiveValues([_waitings]);
   }
 
-  List<WaitingItem> get waitings => _waitings.value;
+  ReactiveList<WaitingItem> get waitings => _waitings;
 
-  void addWaiting(WaitingItem waiting) {
-    // TODO : add to local storage
-    _waitings.value = [..._waitings.value, waiting];
+  // init WaitingStorageService
+  Future<void> init() async {
+    await _waitingStorage.ready;
+
+    try {
+      // load waitings from storage
+      final waitingsFromStorageDynamic = await _waitingStorage.getItem(_waitingsKey);
+      if (waitingsFromStorageDynamic != null) {
+        final waitingsFromStorage = Map<String, WaitingItem>.fromEntries(
+          (waitingsFromStorageDynamic as Map<String, dynamic>).entries.map(
+                (e) => MapEntry(e.key, WaitingItem.fromJson(e.value)),
+          ),
+        );
+        _waitings = ReactiveList.from(waitingsFromStorage.values);
+      }
+    } catch (e) {
+      logger.e('loading waitings from storage error: $e');
+      rethrow;
+    }
+    logger.i('loading waitings from storage completed');
   }
 
-  void removeWaiting(String waitingId) {
-    // TODO : remove from local storage as well
-    _waitings.value = _waitings.value
-        .where((element) => element.waitingId != waitingId)
-        .toList();
+  Future<void> addWaiting(WaitingItem waiting) async {
+    await addWaitingToStorage(waiting);
+    _waitings.add(waiting);
+    logger.i('addWaiting completed');
+    notifyListeners();
+  }
+
+  Future<void> removeWaiting(String waitingId) async {
+    await removeWaitingFromStorage(waitingId);
+    _waitings.removeWhere((element) => element.waitingId == waitingId);
+    logger.i('removeWaiting completed');
+    notifyListeners();
+  }
+
+  Future<void> addWaitingToStorage(WaitingItem waiting) async {
+    final waitingsFromStorageDynamic =
+        await _waitingStorage.getItem(_waitingsKey);
+    if (waitingsFromStorageDynamic != null) {
+      waitingsFromStorageDynamic[waiting.waitingId] = waiting;
+      await _waitingStorage.setItem(_waitingsKey, waitingsFromStorageDynamic);
+    } else {
+      await _waitingStorage.setItem(_waitingsKey, {waiting.waitingId: waiting});
+    }
+  }
+
+  Future<void> removeWaitingFromStorage(String waitingId) async {
+    final waitingsFromStorageDynamic =
+        await _waitingStorage.getItem(_waitingsKey);
+    if (waitingsFromStorageDynamic != null) {
+      waitingsFromStorageDynamic.remove(waitingId);
+      await _waitingStorage.setItem(_waitingsKey, waitingsFromStorageDynamic);
+    }
   }
 }
-
